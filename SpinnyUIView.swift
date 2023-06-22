9146391ba7f65620eb7caf61bbf1765a0bd44af0
CS Prasad <csprasad195@gmail.com>
2023-06-22 19:07:19 +0530

    Initial Push

---
+++ SpinnyUIView.swift
@@ -0,0 +1,66 @@
+//
+//  SwiftUIView.swift
+//  Gradient
+//
+//  Created by CSPrasad on 21/06/23.
+//
+
+import SwiftUI
+
+struct HueRotationView: View {
+    var body: some View {
+         FinalView()
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HueRotationView()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct FinalView: View {
+    @State private var changeColor = false
+    var body: some View {
+        ZStack {
+            Image("5").resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .hueRotation(.degrees(changeColor ? 45 : 1600))
+                .animation(Animation.easeInOut(duration: 5).repeatForever(autoreverses: true))
+                .onAppear() {
+                    self.changeColor.toggle()
+                }.ignoresSafeArea()
+        }
+    }
+}
